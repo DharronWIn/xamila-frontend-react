c4c412b75e960c7fa7b3991d7191acf1a1bda882
Lionel Abrogoua <95973826+DharronWIn@users.noreply.github.com>
2025-10-08 09:32:59 +0000

    deploy github config

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// Remplace NOM_DU_REPO par le nom de ton dépôt GitHub
+const repoName = "xamila-frontend-react";
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: `/${repoName}/`, // <-- Ajouté pour GitHub Pages
   server: {
     host: "::",
     port: 8080,
     allowedHosts: [
       "xamila-frontend-react.onrender.com",
       "xamila-app-backend-nestjs.onrender.com",
-      "localhost", 
+      "localhost",
     ],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
